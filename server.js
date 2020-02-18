6f6fa34f149d1311e3371e3a0cc7d35177dadb9e
christina kaliora <develop@pirron.one>
2020-02-18 13:06:32 +0100

    set up server and accounts-router

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
+const AccountsRouter = require('./accounts/accounts-router'); 
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', AccountsRouter);
+
+server.get('/', (req,res) => {
+    res.send('<h1>Welscome to my Server!</h1>');
+})
 
 module.exports = server;

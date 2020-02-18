428deb953190cff8b12eddfe7f20f46c955cd492
christina kaliora <develop@pirron.one>
2020-02-18 17:10:47 +0100

    finished MVP

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -28,4 +28,45 @@ router.get("/:id", (req, res) => {
         });
 });
 
+router.post("/", (req, res) => {
+    db("accounts")
+        // .insert(req.body, "id")
+        .insert(req.body)
+        .then(account => {
+            res.status(201).json(account)
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "failed to add the account" })
+        });
+});
+
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    db("accounts")
+        .where({ id })
+        .update(changes)
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "failed to update the account" })
+        });
+});
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    db("accounts")
+        .where({ id })
+        .del()
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "failed to delete the account" })
+        });
+});
 module.exports = router;
